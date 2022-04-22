8597c25c1ffa8f6646c10e8b956de1b430db84e4
bhawnarathor1204 <brathor@sebonetechnologies.com>
2022-04-22 13:32:07 +0530

    after again adding bookimpl

--- LibraryManagementProject/LibraryMs.InterfaceImpl/BookImpl.cs
+++ LibraryManagementProject/LibraryMs.InterfaceImpl/BookImpl.cs
@@ -12,7 +12,28 @@ namespace LibraryManagementProject.LibraryMs.InterfaceImpl
     {
         public List<BookResponseDo> getbookDetails()
         {
-            throw new NotImplementedException();
+            List<BookResponseDo> bookDetailList = new List<BookResponseDo>();
+            string path = @"C:\Users\Bhawna Rathor\Desktop\LibBookData.csv";
+            string[] row=System.IO.File.ReadAllLines(path);
+            foreach (string line in row)
+            {
+                String[] split = line.Split(",");
+                foreach (string key in split)
+                {
+                    BookResponseDo bookResObjDo = new BookResponseDo();
+                    bookResObjDo.BookId = Convert.ToInt32(split[0]);
+                    bookResObjDo.BookName = split[1];
+                    bookResObjDo.AuthorName = split[2];
+                    bookResObjDo.IssuedDate = Convert.ToDateTime(split[3]);
+                    bookResObjDo.ReturnDate = Convert.ToDateTime(split[4]);
+                    bookResObjDo.ISBNNumber = Convert.ToInt32(split[5]);
+                    bookResObjDo.BookType = split[6];
+                    bookDetailList.Add(bookResObjDo);
+                }
+            }
+           
+            return bookDetailList;
+
         }
     }
 }
